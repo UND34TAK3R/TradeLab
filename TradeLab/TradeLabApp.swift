1c3cd4443dbb9f35405f6f467953594eb553aeff
Derrick Mangari <dmangari@live.fr>
2025-10-28 21:41:31 -0400

    Initial commit

---
+++ TradeLab/TradeLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeLabApp.swift
+//  TradeLab
+//
+//  Created by Derrick Mangari on 2025-10-28.
+//
+
+import SwiftUI
+
+@main
+struct TradeLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
