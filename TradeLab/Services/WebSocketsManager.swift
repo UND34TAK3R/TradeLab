a109ca75d558c3072952eaae3d321e9b8c6cda95
Derrick Mangari <dmangari@live.fr>
2025-11-18 10:13:43 -0500

    added fields for Holding struct and added WebSocketManager struct

---
+++ TradeLab/Services/WebSocketsManager.swift
@@ -0,0 +1,12 @@
+//
+//  WebSocketsManager.swift
+//  TradeLab
+//
+//  Created by Derrick Mangari on 2025-11-18.
+//
+
+import Foundation
+
+class WebSocketsManager: NSObject, URLSessionWebSocketDelegate{
+    
+}
